9d39f7990e840d41b403d3ce35356fd07ebff97d
PasanSasmika <pasansasmika198@gmail.com>
2024-12-17 08:40:20 +0530

    Setup vite project and tailwind css 🍃

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-green-400 font-extrabold'>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
