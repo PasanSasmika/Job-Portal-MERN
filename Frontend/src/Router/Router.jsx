f73573558c0b9a97cff1dba8461f005999645421
PasanSasmika <pasansasmika198@gmail.com>
2024-12-18 17:42:55 +0530

    Create job post form by using react hook form and react select 🔥

--- Frontend/src/Router/Router.jsx
+++ Frontend/src/Router/Router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
-import About from "../pages/About";
+import PostJob from "../pages/PostJob";
 
 const router = createBrowserRouter([
     {
@@ -9,6 +9,7 @@ const router = createBrowserRouter([
         element: <App/>,
         children:[
             {path: "/", element: <Home/>},
+            {path: "/post-job", element: <PostJob/>},
 
         ]
     },
